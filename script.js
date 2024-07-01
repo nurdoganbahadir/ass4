25af4496ece8ec2d5b0d08cdabef3ee2ef3ef9a4
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-01 16:23:52 +0300

    ürün fiyatları eklendi

---
+++ script.js
@@ -0,0 +1,18 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+
+
+let price1 = "$1599.99";
+let price2 = "$1699.99";
+let price3 = "$1899.99";
+
+button1.onclick = function () {
+  price.textContent = price1;
+};
+button2.onclick = function () {
+  price.textContent = price2;
+};
+button3.onclick = function () {
+  price.textContent = price3;
+};
